a6ca5fb81f9c2cae9d119ac0ae9d6c0f24c370b2
raashna <krishnraashna@gmail.com>
2024-07-07 03:20:43 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const projectData = [
+  {
+    projectName: "Padosan Restaurant",
+    projectDescription:
+      "Padosan Restaurant is a MERN stack website showcasing the restaurant’s offerings and enhancing its online presence.",
+    tags: ["ReactJS", "Nodejs", "Javascript", "MongoDB", "Expressjs"],
+    link: "https://thepadosan.com",
+    repo: "https://github.com/raashna/PadosanRestaurant",
+  },
+  {
+    projectName: "House Price Prediction Model",
+    projectDescription:
+      "Random Forest model to predict house price based on features like area,furnishing status etc..",
+    tags: ["Python", "Jupyter Notebook", "Random Forest"],
+    repo: "https://github.com/raashna/House_Price_Prediction_Model",
+  },
+  {
+    projectName: "Snake Game",
+    projectDescription:
+      "A simple snake game code implement using DSA.",
+    tags: ["CPP", "DSA"],
+    repo: "https://github.com/raashna/snake_game_dsa",
+  },
+  {
+    projectName: "GOOGLE AUTHENTICATION WITH FIREBASE",
+    projectDescription: "Login Flow App to provide a user-friendly authentication and authorization system through Firebas.",
+    tags: ["Android", "Kotlin", "Firebase"],
+    repo: "https://github.com/raashna/GoogleSignIn",
+  },
+  {
+    projectName: "PDF Viewer",
+    projectDescription: "simple PDF viewer android app to store and view any pdf",
+    tags: ["Kotlin", "Android"],
+    repo: "https://github.com/raashna/CodeClause_PDF_VIEWER",
+  },
+  {
+    projectName: "Calculator App",
+    projectDescription:
+      "A simple calculator app.",
+    tags: ["Kotlin", "Android"],
+    repo: "https://github.com/raashna/Calculator-app",
+  },
+];
